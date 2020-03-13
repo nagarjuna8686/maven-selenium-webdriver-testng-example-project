cf17f1b58e36b51ed9d66c5dfa04afc9088965ee
nagarjuna8686 <34602673+nagarjuna8686@users.noreply.github.com>
2020-03-13 16:10:41 +0530

    Update WebdriverSettings.java

--- src/test/java/testcases/mytheresa/WebdriverSettings.java
+++ src/test/java/testcases/mytheresa/WebdriverSettings.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class WebdriverSettings {
 	
 	public WebDriver driverSettings() {
+		String nodeURL1 = "http://54.80.92.125:4444//wd/hub";
 		System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
 		WebDriver driver = new ChromeDriver();
 		return driver;
