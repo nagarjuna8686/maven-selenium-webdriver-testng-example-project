3c27a7bb9b3e50ba3788ee112eabde2ebc950183
nagarjuna8686 <34602673+nagarjuna8686@users.noreply.github.com>
2020-03-16 11:53:24 +0530

    Update WebdriverSettings.java

--- src/test/java/testcases/mytheresa/WebdriverSettings.java
+++ src/test/java/testcases/mytheresa/WebdriverSettings.java
@@ -2,11 +2,18 @@ package testcases.mytheresa;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options;
 
 public class WebdriverSettings {
 	
 	public WebDriver driverSettings() {
-		System.setProperty("webdriver.chrome.driver", "lib/chromedriver");
+		
+		chrome_options = Options()
+                chrome_options.add_argument("--headless")
+                chrome_options.add_argument('--no-sandbox')
+		driver = webdriver.Chrome('lib/chromedriver', options=chrome_options)
+		//System.setProperty("webdriver.chrome.driver", "lib/chromedriver");
 		WebDriver driver = new ChromeDriver();
 		return driver;
 	}
